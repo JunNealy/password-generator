aea15ff03487eeec2b50509d0ef2c0d30dede499
TNealy <66037350+JunNealy@users.noreply.github.com>
2024-02-26 14:40:53 -0500

    state update logic + password generation

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 // character collections
-const alphabetChars = 'abcdefghijklmnopqrstuvwxyz';
+const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const numberChars = '1234567890';
 const symbolChars = '!"#$%&()*+,-./:;<>=?@[]^_`{}|~';
 
-// event listeners
+//Option Elements
 const uppercase = document.getElementById('uppercase');
 const lowercase = document.getElementById('lowercase');
 const symbols = document.getElementById('symbols');
 const numbers = document.getElementById('numbers');
+const characterLength = document.getElementById('character-length');
+const generate = document.getElementById('generate');
 
 // state management
 let state = {
-  length: 0,
+  length: 10,
   uppercase: false,
   lowercase: false,
   numbers: false,
@@ -26,8 +29,51 @@ function getState() {
   return state;
 }
 
-//
-uppercase.addEventListener('click', function () {
-  updateState(uppercase, uppercase.checked);
+//State Change
+function addListenerAndUpdate(element, option) {
+  element.addEventListener('click', function () {
+    let newState = element.checked;
+    updateState(option, newState);
+    console.log(getState());
+  });
+}
+
+addListenerAndUpdate(uppercase, 'uppercase');
+addListenerAndUpdate(lowercase, 'lowercase');
+addListenerAndUpdate(symbols, 'symbols');
+addListenerAndUpdate(numbers, 'numbers');
+
+characterLength.addEventListener('change', function () {
+  let newState = characterLength.value;
+  updateState('length', newState);
   console.log(getState());
+});
+
+function concat(state) {
+  let { uppercase, lowercase, symbols, numbers } = state;
+  let potentialChars = '';
+  if (lowercase) {
+    potentialChars += lowercaseChars;
+  }
+  if (uppercase) {
+    potentialChars += uppercaseChars;
+  }
+  if (symbols) {
+    potentialChars += symbolChars;
+  }
+  if (numbers) {
+    potentialChars += numberChars;
+  }
+  return potentialChars;
+}
+
+generate.addEventListener('click', function () {
+  let passwordLength = state['length'];
+  let charString = concat(state);
+  let password = '';
+  for (let i = 0; i < passwordLength; i++) {
+    let character = Math.floor(Math.random() * charString.length);
+    password += charString[character];
+  }
+  console.log(password);
 });
